171f79820e1470cef763e64224dffab397f0a39b
Amin Sabri <aminnnsbri05@gmail.com>
2026-03-16 23:29:12 +0800

    Learn: adding game title.

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -4,6 +4,7 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
 running = True
 
 while running:
